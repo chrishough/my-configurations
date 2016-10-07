944fc16e73224f70af6428d98d5316eff36f19c7
Chris Hough <chris@hough.studio>
2016-10-07 03:07:44 -0700

    adjust symlinks

--- scripts/setup/dotfiles/symlinks.rb
+++ scripts/setup/dotfiles/symlinks.rb
@@ -2,13 +2,13 @@ require 'fileutils'
 require_relative 'settings'
 include Settings
 
-def remove_installed_file
+def remove_installed_file(filename)
   FileUtils.rm(@curr_dot_file)
   puts "#{filename} has been successfully removed..."
 end
 
 def create_dot_file_shortcut(filename)
-  remove_installed_file if File.exist?(@curr_dot_file)
+  remove_installed_file(filename) if File.exist?(@curr_dot_file)
   FileUtils.ln_s(@new_dot_file, @curr_dot_file, force: true)
   puts "#{filename} shortcut has been created successfully..."
 end
