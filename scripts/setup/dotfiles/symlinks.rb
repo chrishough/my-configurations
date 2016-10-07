32a70f90b14009043ee393284364e7b84009854c
Chris Hough <chris@hough.studio>
2016-10-07 03:06:41 -0700

    adjust symlinks

--- scripts/setup/dotfiles/symlinks.rb
+++ scripts/setup/dotfiles/symlinks.rb
@@ -2,15 +2,15 @@ require 'fileutils'
 require_relative 'settings'
 include Settings
 
+def remove_installed_file
+  FileUtils.rm(@curr_dot_file)
+  puts "#{filename} has been successfully removed..."
+end
+
 def create_dot_file_shortcut(filename)
-  begin
-    FileUtils.rm(@curr_dot_file)
-    puts "#{filename} has been successfully removed..."
-    FileUtils.ln_s(@new_dot_file, @curr_dot_file, force: true)
-    puts "#{filename} shortcut has been created successfully..."
-  rescue
-    puts "Error! '#{filename}' back shortcut can not be created! SKIPPED..."
-  end
+  remove_installed_file if File.exist?(@curr_dot_file)
+  FileUtils.ln_s(@new_dot_file, @curr_dot_file, force: true)
+  puts "#{filename} shortcut has been created successfully..."
 end
 
 DOT_FILE_LIST.each do |filename|
