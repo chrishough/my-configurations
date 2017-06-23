b3b8f497ed8e6513890962c369f2da1f8556e473
Chris Hough <tech@aboutchrishough.com>
2017-06-23 12:08:54 -0700

    add config window config

--- scripts/setup/apps/symlinks.rb
+++ scripts/setup/apps/symlinks.rb
@@ -4,7 +4,7 @@ def create_itermocil
   app_dir = File.join(Dir.home, '.myconfigurations', 'workstation', 'itermocil')
   dofile_shortcut =  File.join(Dir.home, '.itermocil')
   FileUtils.ln_s(app_dir, dofile_shortcut, force: true)
-  puts "itermocil} shortcut has been created successfully..."
+  puts "itermocil shortcut has been created successfully..."
 end
 
 create_itermocil
