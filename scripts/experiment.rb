9f9806ac9776de773a4803ba3122bc8050d789c4
Chris Hough <tech@aboutchrishough.com>
2018-09-24 14:26:38 -0700

    add indicator and endwise to vscode

--- scripts/experiment.rb
+++ scripts/experiment.rb
@@ -1,1 +1,28 @@
 # Saved for ALL the SPIKES!
+
+# Testing https://marketplace.visualstudio.com/items?itemName=kaiwood.endwise
+def test 
+  array.each dp |x| do
+
+  end
+end
+
+# Testing https://marketplace.visualstudio.com/items?itemName=SirTori.indenticator
+{
+  test: {
+    test: {
+      test: {
+        test: {
+          test: 1,
+          test: 1,
+          test: 1,
+          test: 1,
+          test: 1,
+          test: 1,
+          test: 1,
+          test: 1                                                                    
+        }        
+      }      
+    }    
+  }  
+}
