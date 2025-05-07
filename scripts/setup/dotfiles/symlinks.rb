a9cd2c533e8b00b2617c6a433cef35d94f740989
Chris Hough <tech@aboutchrishough.com>
2025-05-07 06:46:21 -0700

    update settings

--- scripts/setup/dotfiles/symlinks.rb
+++ scripts/setup/dotfiles/symlinks.rb
@@ -2,12 +2,15 @@ require 'fileutils'
 require_relative 'settings'
 include Settings
 
+# ln -s "$HOME/.myconfigurations" "$HOME/Documents/myconfigurations"
+
 # sudo codesign --force --deep --sign - /Applications/Visual\ Studio\ Code.app
 # ln -s ~/.myconfigurations/workstation/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 # ln -s ~/.myconfigurations/workstation/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
 # ln -s "$HOME/.myconfigurations/workstation/windsurf/settings.json" "$HOME/Library/Application Support/Windsurf/User/settings.json"
 # ln -s "$HOME/.myconfigurations/workstation/windsurf/keybindings.json" "$HOME/Library/Application Support/Windsurf/User/keybindings.json"
+# ln -s "$HOME/.myconfigurations/workstation/windsurf/snippets/ruby.json" "$HOME/Library/Application Support/Windsurf/User/snippets/ruby.json"
 
 # sudo codesign --force --deep --sign - /Applications/Cursor.app
 # ln -s "$HOME/.myconfigurations/workstation/cursor/settings.json" "$HOME/Library/Application Support/Cursor/User/settings.json"
