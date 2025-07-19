b40d4218e2b8ae33b573b4d06826b1d1081f324d
Chris Hough <tech@aboutchrishough.com>
2025-07-18 19:17:42 -0700

    update settings

--- scripts/setup/dotfiles/symlinks.rb
+++ scripts/setup/dotfiles/symlinks.rb
@@ -28,9 +28,9 @@ include Settings
 # cursor --list-extensions > "/Users/christopherhough/Library/Application Support/Cursor/User/extensions.txt"
 # xargs -n1 cursor --install-extension < "/Users/christopherhough/Library/Application Support/Cursor/User/extensions.txt"
 
-# cursor --install-extension vscode-theme-railscasts-plus-0.0.6.vsix
+# cursor --install-extension vscode-theme-railscasts-plus/vscode-theme-railscasts-plus-0.0.6.vsix
 # cursor --install-extension misogi.ruby-rubocop-0.8.5.vsix
-# cursor --install-extension markdown-emoji-0.3.1.vsix
+# cursor --install-extension vscode-markdown-emoji/markdown-emoji-0.3.1.vsix
 
 
 # cursor --list-extensions \
