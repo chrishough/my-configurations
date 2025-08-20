b6b337074ebe9d937da1105f06190b15273f8d18
Chris Hough <tech@aboutchrishough.com>
2025-08-20 06:30:53 -0700

    refactor & clean up sourced shell functions

---
+++ lib/install.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+# sh "$HOME/.myconfigurations/lib/install.sh"
+
+brew install autojump
+brew install fzf
+brew install itermocil
+brew install heroku
+brew install postgresql
+brew install python
+brew install ruby
+brew install zsh
+brew install zsh-completions
+brew install zsh-syntax-highlighting
+brew install zsh-autosuggestions
