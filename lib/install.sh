2e46f05b612f29deb0b5cbad2872e8d27a9616e4
Chris Hough <tech@aboutchrishough.com>
2025-08-20 18:57:12 -0700

    refactor & clean up sourced shell functions

--- lib/install.sh
+++ lib/install.sh
@@ -1,14 +1,40 @@
 #!/bin/zsh
 # sh "$HOME/.myconfigurations/lib/install.sh"
 
-brew install autojump
-brew install fzf
-brew install itermocil
-brew install heroku
-brew install postgresql
-brew install python
-brew install ruby
-brew install zsh
-brew install zsh-completions
-brew install zsh-syntax-highlighting
-brew install zsh-autosuggestions
+# Function to install brew packages if they don't exist
+install_brew_packages() {
+    # Array of packages to install
+    local packages=(
+        "autojump"
+        "fzf"
+        "itermocil"
+        "heroku"
+        "postgresql"
+        "python"
+        "ruby"
+        "zsh"
+        "zsh-completions"
+        "zsh-syntax-highlighting"
+        "zsh-autosuggestions"
+    )
+    
+    echo "Running brew update..."
+    brew update
+    
+    # Loop through packages and install if not present
+    for package in "${packages[@]}"; do
+        if brew list --formula | grep -q "^${package}\$"; then
+            echo "✓ ${package} is already installed"
+        else
+            echo "Installing ${package}..."
+            brew install "${package}"
+        fi
+    done
+    
+    echo "Running brew update and doctor..."
+    brew update
+    brew doctor
+}
+
+# Run the installation function
+install_brew_packages
