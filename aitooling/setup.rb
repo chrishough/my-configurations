0e1831ea33a02b8610d748794deae98aa7ab4111
Chris Hough <tech@aboutchrishough.com>
2026-02-02 14:35:22 -0800

    extract ai settings

--- aitooling/setup.rb
+++ aitooling/setup.rb
@@ -4,11 +4,11 @@ PATHS.concat([
     claude: [
       {
         source: "$HOME/.claude/settings.json",
-        destination: "$HOME/.myconfigurations/aitooling/claude/settings.json"
+        destination: "$HOME/.myconfigurations.ai/claude/settings.json"
       },
       {
         source: "$HOME/.claude/CLAUDE.md",
-        destination: "$HOME/.myconfigurations/aitooling/claude/CLAUDE.md"
+        destination: "$HOME/.myconfigurations.ai/claude/brains/global/CLAUDE.md"
       }
     ]
   }
